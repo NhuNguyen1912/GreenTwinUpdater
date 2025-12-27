1f8a5d2c140b15e95b361a0ac88c489f8d87c638
quinnhu <nnquynhnhu1912@gmail.com>
2025-12-27 10:46:35 +0700

    Update next.config.mjs

--- GreenTwinAppUI/next.config.mjs
+++ GreenTwinAppUI/next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  module.exports = nextConfig;
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
